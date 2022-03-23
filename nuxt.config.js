1d438ca5c9bf8429f44fe900adb5dc86200cd51d
Valentin Chantelot <vchantelot@turbulent.ca>
2022-03-23 11:07:01 +0100

    Remove useless lib

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,36 +1,35 @@
 export default {
-  head: {
-    title: 'vite-nuxt-template',
-    htmlAttrs: {
-      lang: 'en',
+    head: {
+        title: 'vite-nuxt-template',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: ''},
+            {name: 'format-detection', content: 'telephone=no'}
+        ],
+        link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+
+    // Global CSS to load everywhere
+    css: ['~assets/style/main.scss'],
+    styleResources: {
+        scss: ['~assets/style/mixins.scss', '~assets/style/variables.scss']
+    },
+
+    plugins: [],
+    components: true,
+    buildModules: [
+        '@nuxtjs/eslint-module',
+        '@nuxtjs/style-resources',
+        '@nuxtjs/svg',
+        '@nuxt/image'
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
-
-  // Global CSS to load everywhere
-  css: ['~assets/style/main.scss'],
-  styleResources: {
-    scss: ['~assets/style/mixins.scss', '~assets/style/variables.scss'],
-  },
-
-  plugins: [],
-  components: true,
-  buildModules: [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/svg',
-    '@braid/vue-formulate/nuxt',
-    '@nuxt/image',
-  ],
-  modules: ['@nuxtjs/axios'],
-  axios: {
-    baseURL: '/',
-  },
-  build: {},
-}
+    modules: ['@nuxtjs/axios'],
+    axios: {
+        baseURL: '/'
+    },
+    build: {}
+};
