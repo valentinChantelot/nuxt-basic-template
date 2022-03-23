d2b796f0626b9df6cfd5b04ed24219349aa108dc
Valentin Chantelot <vchantelot@turbulent.ca>
2022-03-23 10:58:28 +0100

    Init basic nuxt 2 template

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+export default {
+  head: {
+    title: 'vite-nuxt-template',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS to load everywhere
+  css: ['~assets/style/main.scss'],
+  styleResources: {
+    scss: ['~assets/style/mixins.scss', '~assets/style/variables.scss'],
+  },
+
+  plugins: [],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/svg',
+    '@braid/vue-formulate/nuxt',
+    '@nuxt/image',
+  ],
+  modules: ['@nuxtjs/axios'],
+  axios: {
+    baseURL: '/',
+  },
+  build: {},
+}
